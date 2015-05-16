0e0a745670f8840da95fef7d97ef9f54c1bd71ae
jvechinski <jvechinski@gmail.com>
2015-05-16 16:37:36 -0500

    Add file header to source files.

---
+++ source/jv_json.hpp
@@ -0,0 +1,44 @@
+//////////////////////////////////////////////////////////////////////
+/// @file
+///
+/// Master include file for the JV JSON parser.  This is the file 
+/// users should be including to get definitions of all the public
+/// classes.
+///
+/// Part of the JV JSON parser project, 
+/// https://github.com/jvechinski/jv_json
+/// Released under the MIT license.
+///
+/// @ingroup jv_json
+///
+/// Copyright (c) 2015 Jeremy S. Vechinski
+///
+/// Permission is hereby granted, free of charge, to any person 
+/// obtaining a copy of this software and associated documentation 
+/// files (the "Software"), to deal in the Software without restriction, 
+/// including without limitation the rights to use, copy, modify, merge, 
+/// publish, distribute, sublicense, and/or sell copies of the Software, 
+/// and to permit persons to whom the Software is furnished to do so, 
+/// subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be 
+/// included in all copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+/// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+/// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+/// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS 
+/// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+/// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+/// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+/// SOFTWARE.
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(JVJSON_JVJSON_HPP)
+#define JVJSON_JVJSON_HPP
+
+#include "document.hpp"
+#include "element.hpp"
+#include "exception.hpp"
+
+#endif
